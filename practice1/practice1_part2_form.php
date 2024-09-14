44c8b32319175950db01cc1e72c6b2f6b83b4c12
s.berkunov <studiorain27@gmail.com>
2024-09-14 11:04:24 +0300

    feat(practice1): add 2 part

---
+++ practice1/practice1_part2_form.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <style>
+        .form {
+            display: flex;
+            flex-direction: column;
+            gap: 8px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Оцените качество предоставляемых услуг!</h1>
+
+    <form action="practice1_part2_answer.php" class="form">
+        <label>
+            Мяу?
+            <input type="text">
+        </label>
+    </form>
+</body>
+</html>
---
+++ router.php
@@ -0,0 +1,25 @@
+<?php
+
+$request = $_SERVER['REQUEST_URI'];
+$viewDir = '/';
+
+switch ($request) {
+    case '':
+    case '/':
+        require __DIR__ . $viewDir . 'index.php';
+        break;
+
+    case '/practice1':
+        require __DIR__ . $viewDir . 'practice1/' . 'practice1_part2_form.php';
+        break;
+
+    case '/practice1/answer':
+        require __DIR__ . $viewDir . 'practice1/' . 'practice1_part2_answer.php';
+        break;
+
+    default:
+        http_response_code(404);
+        require __DIR__ . $viewDir . '404.php';
+}
+
+?>
